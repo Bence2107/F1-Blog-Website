71a377a7e04cea98478579349e99707c3bfc2659
Bence2107 <szabobence003@gmail.com>
2025-03-29 18:20:13 +0100

    Defined basic routes & making app.routes.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {AppComponent} from './app.component';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    AppComponent,
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {NewsComponent} from './pages/news/news.component';
+import {SignupComponent} from './pages/auth/signup/signup.component';
+import {LoginComponent} from './pages/auth/login/login.component';
+import {ProfileComponent} from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: "home", component: HomeComponent },
+
+  { path: "news", component: NewsComponent },
+
+  { path: "login", component: LoginComponent },
+
+  { path: "signup", component: SignupComponent },
+
+  { path: "profile", component: ProfileComponent },
+
+
+];
