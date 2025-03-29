fa800568b43ec12496d18985483584698167fa4f
Bence2107 <szabobence003@gmail.com>
2025-03-29 18:27:13 +0100

    Header & Footer Design

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {NgClass, NgForOf} from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  imports: [
+    NgForOf,
+    NgClass,
+  ],
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  currentYear: number = new Date().getFullYear();
+
+  socialLinks = [
+    { label: 'Formula 1 Official Website', url: 'https://www.formula1.com', icon: 'fas fa-globe' },
+    { label: 'F1 Twitter', url: 'https://twitter.com/F1', icon: 'fab fa-twitter' },
+    { label: 'F1 Instagram', url: 'https://instagram.com/f1', icon: 'fab fa-instagram' },
+    { label: 'F1 YouTube', url: 'https://www.youtube.com/F1', icon: 'fab fa-youtube' },
+    { label: 'F1 Facebook', url: 'https://www.facebook.com/Formula1', icon: 'fab fa-facebook' }
+  ];
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink,
+    NgOptimizedImage,
+    MatButton
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
