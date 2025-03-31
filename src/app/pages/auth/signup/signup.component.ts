80ce9f8996f05d3670797bc3b2e78eed13725526
Bence2107 <szabobence003@gmail.com>
2025-03-31 21:13:39 +0200

    Move Header & Footer globally to the app.component

--- src/app/pages/auth/signup/signup.component.ts
+++ src/app/pages/auth/signup/signup.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from '../../../components/header/header.component';
 
 @Component({
   selector: 'app-signup',
-  imports: [
-    HeaderComponent,
-  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from '../../components/header/header.component';
-import {FooterComponent} from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-profile',
   imports: [
-    HeaderComponent,
-    FooterComponent
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
