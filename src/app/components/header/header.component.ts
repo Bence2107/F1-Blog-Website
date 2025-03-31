14469512309a99ee0d176d73c84434fe697ecdf4
Bence2107 <szabobence003@gmail.com>
2025-03-31 17:37:22 +0200

    Making SideBar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,36 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {NgOptimizedImage} from '@angular/common';
+import {NgIf, NgOptimizedImage} from '@angular/common';
 import {MatButton} from '@angular/material/button';
+import {MatSidenav, MatSidenavContainer, MatSidenavModule} from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
-    NgOptimizedImage,
-    MatButton
+    MatButton,
+    MatSidenavContainer,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenav,
+    NgIf
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isScreenSmall: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe(['(max-width: 870px)']).subscribe(result => {
+      this.isScreenSmall = result.matches;
+    });
+  }
 
 }
