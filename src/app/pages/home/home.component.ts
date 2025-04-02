2c1421905135f99535c98f172dc5c522f08bdd57
Bence2107 <szabobence003@gmail.com>
2025-04-02 21:26:47 +0200

    Add Primary news to Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
+    CommonModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  news_list = [
+    { id: "1", label: 'Fehérbe borul a Red Bull', desc: "Japán versenyző, japán festés" },
+    { id: "2", label: 'Áttörést találhatott a Ferrari', desc: "20 különböző szimulátor tapasztalat után visszatérhetnek a Pirosak?" },
+    { id: "3", label: 'Lawsont sokként érte a Red Bull döntése', desc: " Tsunoda adott esetben legyőzheti Verstappent" },
+  ];
 }
