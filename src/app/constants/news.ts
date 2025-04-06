cf9a2212412bc64ab1be9c0008820401350952af
Bence2107 <szabobence003@gmail.com>
2025-04-06 20:35:26 +0200

    Rework news constant

--- src/app/constants/news.ts
+++ src/app/constants/news.ts
@@ -1,55 +1,49 @@
-import {NewsModel} from '../models/news_model';
+import {NewsListModel} from '../models/news_list_model';
 
-export const primary_news_list : NewsModel[] = [
+export const primary_news_list : NewsListModel[] = [
   {
-    id: "1",
+    id: 4,
+    url: "verstappen-nyert-japan-festesben",
+    label: "Verstappen nyerte a 2025-ös Japán nagydíjat",
+    summary: "Szerencsét hozott a fehér festés",
+  },
+  {
+    id: 1,
     url: "japan-versenyzo-japan-festes",
     label: "Fehérbe borul a Red Bull",
-    description: "Japán versenyző, japán festés",
-    first_section: "A Red Bull új különkiadású festéssel tiszteleg a japán kultúra előtt. Az autó fehér alapszínt kapott, piros akcentusokkal.",
-    second_section: "A festés a Suzukai Nagydíj hétvégéjén lesz látható. A japán rajongók már most imádják az új dizájnt."
+    summary: "Japán versenyző, japán festés"
   },
   {
-    id: "2",
+    id: 2,
     url: "ferrari-25-attores",
     label: "Áttörést találhatott a Ferrari",
-    description: "20 különböző szimulátor tapasztalat után visszatérhetnek a Pirosak?",
-    first_section: "A Ferrari mérnökei több hónapos tesztelés után új megoldásokat találtak a leszorítóerő optimalizálására.",
-    second_section: "A változtatások elsőként Imolában debütálnak. Leclerc bizakodó a szezon második felére nézve."
+    summary: "20 különböző szimulátor tapasztalat után visszatérhetnek a Pirosak?",
   },
-  {
-    id: "3",
-    url: "lawson-red-bull-sokk",
-    label: "Lawsont sokként érte a Red Bull döntése",
-    description: "Tsunoda adott esetben legyőzheti Verstappent",
-    first_section: "Liam Lawson szerint meglepő és kiábrándító volt a Red Bull döntése, miszerint nem ő kap lehetőséget 2025-re.",
-    second_section: "A fiatal új-zélandi pilóta a Super Formulában bizonyít, miközben továbbra is az F1-es visszatérésre készül."
-  }
 ];
 
-export const news_list_list : NewsModel[] = [
+export const news_list : NewsListModel[] = [
   {
-    id: "1",
+    id: 4,
+    url: "verstappen-nyert-japan-festesben",
+    label: "Verstappen nyerte a 2025-ös Japán nagydíjat",
+    summary: "Szerencsét hozott a fehér festés",
+  },
+  {
+    id: 1,
     url: "japan-versenyzo-japan-festes",
     label: "Fehérbe borul a Red Bull",
-    description: "Japán versenyző, japán festés",
-    first_section: "A Red Bull új különkiadású festéssel tiszteleg a japán kultúra előtt. Az autó fehér alapszínt kapott, piros akcentusokkal.",
-    second_section: "A festés a Suzukai Nagydíj hétvégéjén lesz látható. A japán rajongók már most imádják az új dizájnt."
+    summary: "Japán versenyző, japán festés",
   },
   {
-    id: "2",
+    id: 2,
     url: "ferrari-25-attores",
     label: "Áttörést találhatott a Ferrari",
-    description: "20 különböző szimulátor tapasztalat után visszatérhetnek a Pirosak?",
-    first_section: "A Ferrari mérnökei több hónapos tesztelés után új megoldásokat találtak a leszorítóerő optimalizálására.",
-    second_section: "A változtatások elsőként Imolában debütálnak. Leclerc bizakodó a szezon második felére nézve."
+    summary: "20 különböző szimulátor tapasztalat után visszatérhetnek a Pirosak?",
   },
   {
-    id: "3",
+    id: 3,
     url: "lawson-red-bull-sokk",
     label: "Lawsont sokként érte a Red Bull döntése",
-    description: "Tsunoda adott esetben legyőzheti Verstappent",
-    first_section: "Liam Lawson szerint meglepő és kiábrándító volt a Red Bull döntése, miszerint nem ő kap lehetőséget 2025-re.",
-    second_section: "A fiatal új-zélandi pilóta a Super Formulában bizonyít, miközben továbbra is az F1-es visszatérésre készül."
-  }
+    summary: "Tsunoda adott esetben legyőzheti Verstappent",
+  },
 ];
