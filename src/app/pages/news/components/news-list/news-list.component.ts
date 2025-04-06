b6b440e67780150ec7f75d4e3c5bb56aadd903a8
Bence2107 <szabobence003@gmail.com>
2025-04-06 20:34:05 +0200

    Rework News Site

---
+++ src/app/pages/news/components/news-list/news-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {news_list} from '../../../../constants/news';
+import {RouterLink} from '@angular/router';
+import {NgForOf} from '@angular/common';
+
+@Component({
+  selector: 'app-news-list',
+  imports: [
+    RouterLink,
+    NgForOf
+  ],
+  templateUrl: './news-list.component.html',
+  styleUrl: './news-list.component.scss'
+})
+export class NewsListComponent {
+
+  protected readonly news_list = news_list;
+}
--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {NewsListComponent} from './components/news-list/news-list.component';
 
 @Component({
   selector: 'app-news',
   imports: [
+    NewsListComponent
   ],
   templateUrl: './news.component.html',
   styleUrl: './news.component.scss'
