56b3264b68912accb834bbce6a02861d4423498e
Bence2107 <szabobence003@gmail.com>
2025-04-06 20:37:35 +0200

    Change model for News Listing

--- src/app/pages/home/components/primary-news-list/primary-news-list.component.ts
+++ src/app/pages/home/components/primary-news-list/primary-news-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {NgForOf} from '@angular/common';
-import {NewsModel} from '../../../../models/news_model';
+import {NewsListModel} from '../../../../models/news_list_model';
 import {primary_news_list} from '../../../../constants/news';
 import {RouterLink} from '@angular/router';
 
@@ -14,7 +14,7 @@ import {RouterLink} from '@angular/router';
   styleUrl: './primary-news-list.component.scss'
 })
 export class PrimaryNewsListComponent {
-  @Input() news_list: NewsModel[] | undefined;
+  @Input() news_list: NewsListModel[] | undefined;
 
   ngOnInit() {
     if (!this.news_list) {
