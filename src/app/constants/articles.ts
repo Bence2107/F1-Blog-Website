31d9ef38950cf412772a0aab46b30433fcd534ee
Bence2107 <szabobence003@gmail.com>
2025-04-12 23:26:27 +0200

    Write example dummy's

--- src/app/constants/articles.ts
+++ src/app/constants/articles.ts
@@ -6,8 +6,13 @@ export const news_article_list: ArticleModel[] = [
     url: "japan-versenyzo-japan-festes",
     title: "Fehérbe borul a Red Bull",
     summary: "Japán versenyző, japán festés",
-    first_section: "Szép a fehér red bull",
-    last_section: "Nyert is",
+    first_section: "<p>Év végén búcsúzó motorpartnere, a Honda előtt tiszteleg különleges festésével a Japán Nagydíj hétvégéjén a Red Bull F1-es csapata. \n" +
+      "      \"A sötétkék színt átmenetileg felváltja a hófehér, visszahozva az RA272-es, első Honda győzelmet arató autó festését.</p>",
+    last_section:
+      "<blockquote>„A festéssel újrateremtettük az RA272-es ikonikus japán zászlós motívumát, látható rajta a H-logó az orron, az egyszerű Honda-logó a kocsitest hátulján. Egy különleges logóval a Honda első F1-es győzelmének 60. évfordulója előtt is tisztelgünk a Red Bull-autókon”\n" +
+      "– írta a Red Bull a közleményében.\n </blockquote>" +
+      "<p>A csapat öltözékét is a fehér szín uralja, ezzel méltó búcsút mondva a japán gyártónak. Tsunoda a különleges autófestés mellé egyedi sisakfestéssel is készül hazai, egyben Red Bull debütáló hétvégéjére is.\n</p>" +
+      "<p>Az ünneplés ezzel még csak elkezdődik. A folytatást helyi idő szerint kedd este egy Red Bull-os tokiói utcai bemutató jelenti, melynek témáját az autókultúra képezi. Az eseményen többek között egy Honda NSX-et is bemutatnak a Red Bull RB21-es új festésébe öltöztetve. Szerdán aztán a Red Bull négy F1-es versenyzője, Max Verstappen, az újonnan előléptetett hazai hős. Cunoda Juki, valamint a Racing Bullshoz vezényelt Liam Lawson és az újonc Isack Hadjar is autóba ül, hogy bevegye Tokió utcáit. A Honda RA272-es, a Verstappen első bajnoki címét hozó Red Bull RB16B, valamint két RB7-es fog száguldozni. Ez lesz Cunoda és Lawson első nyilvános autós fellépése az új/régi-új csapata színeiben.\n</p>",
     date: "2025.04.02.",
     isReview: false
   },
@@ -17,9 +22,9 @@ export const news_article_list: ArticleModel[] = [
     title: "Áttörést találhatott a Ferrari",
     summary: "20 különböző szimulátor tapasztalat után visszatérhetnek a Pirosak?",
     first_section:
-      "<p>Az olasz <i>Gazetta dello Sport</i> információi szerint a Ferrari szimulátorban komolyabb áttörésre tehetett szert, ami a 2025-ös szezon során az autójuk felerősödhet és nagyobb esélyekkel szállhat versenybe a konstruktőri, de akár az egyéni világbajnoki címért. " +
-      "Minden kezdet nehéz, ahogy szokták mondani. Ez a Ferrarinál sincs másképp: Ausztráliában alig szerzett pontot a csapat, a kínai nagydíjon pedig mindkét pilótát kizárták technikai okokra hivatkozva. " +
-      "A szezon harmadik hétvégéjére, a Japán Nagydíjra másfél hét készülődés jutott neki, amit úgy látszik ki is használtak, ugyanis a szimulátorban végzett kemény munkának köszönhetően megoldást találtak a beállítással kapcsolatos problémákra. A lap úgy nyilatkozott, az olasz csapat ez idő alatt 20 különböző szimulátor beállítást tesztelt, amivel megoldást találhatnak az autók problémáira. </p><br>",
+      "<p>Az olasz <i>Gazetta dello Sport</i> információi szerint a Ferrari szimulátorban komolyabb áttörésre tehetett szert, ami a 2025-ös szezon során az autójuk felerősödhet és nagyobb esélyekkel szállhat versenybe a konstruktőri, de akár az egyéni világbajnoki címért. </p>" +
+      "<p>Minden kezdet nehéz, ahogy szokták mondani. Ez a Ferrarinál sincs másképp: Ausztráliában alig szerzett pontot a csapat, a kínai nagydíjon pedig mindkét pilótát kizárták technikai okokra hivatkozva. </p>" +
+      " szezon harmadik hétvégéjére, a Japán Nagydíjra másfél hét készülődés jutott neki, amit úgy látszik ki is használtak, ugyanis a szimulátorban végzett kemény munkának köszönhetően megoldást találtak a beállítással kapcsolatos problémákra. A lap úgy nyilatkozott, az olasz csapat ez idő alatt 20 különböző szimulátor beállítást tesztelt, amivel megoldást találhatnak az autók problémáira. </p><br>",
     last_section: "A csapat biztos abban, hogy az idei autónak a tervezése rendben van, így bizakodóan állnak hozzá a suzukai hétvégéhez. A szezon során több fejlesztési csomagot is bejelentett a Ferrari, egyet Bahreinben, egyet pedig a Miami hétvége idején. ",
     date: "2025.04.01.",
     isReview: false
@@ -29,8 +34,12 @@ export const news_article_list: ArticleModel[] = [
     url: "lawson-red-bull-sokk",
     title: "Lawsont sokként érte a Red Bull döntése",
     summary: "Tsunoda adott esetben legyőzheti Verstappent",
-    first_section: "Nem tudja",
-    last_section: "Nem elég szép a Red Bull",
+    first_section: "<p>Lawson Red Bull karrierje mindössze két hétvégét élt meg, amíg helyet nem kellett cserélni a Racing Bulls japán versenyzőjével.</p>" +
+      "<p>A szezonkezdő teljesítménye miatt összeült a Red Bull vezetősége és döntöttek: Lawson helyére ültetik Tsunodát. Várható lépés volt ez a csapat részéről, sokak számára mégis megdöbbentő volt – köztük az új-zélandi pilóta számára is.</p>" +
+      "<blockquote>„Ez egyértelműen sokkolt, hogy őszinte legyek. Nem láttam jelét. Az egyeztetéseink jól mentek, és szerintem nem éppen ebbe az irányba mutattak, szóval határozottan nem számítottam ilyesmire”\n" +
+      "– ismerte el a Sky Sportsnak nyilatkozó Lawson.</blockquote>",
+    last_section: "<p>A csapat úgy gondolta, hogy a versenyző még nem áll készen a Red Bull autóját vezetni, így Lawson érdekében is jobb, ha a Racing Bulls ülését tölti be, ezzel kisebb kényszernek kitéve.</p>" +
+      "<p>Egyesek szerint az autót nehéz vezetni, Lawson szerint csak több idő kellett volna neki, hogy az autót magabiztosan tudja vezetni. Továbbá érdemes megjegyezni, a Red Bull második ülését sokszor passzolják a pilóták között, így például a spanyol Carlos Sainz sem lepődött meg a csapat döntésén.</p>",
     date: "2025.03.31.",
     isReview: false
   },
@@ -39,8 +48,11 @@ export const news_article_list: ArticleModel[] = [
     url: "verstappen-nyert-japan-festesben",
     title: "Verstappen nyerte a 2025-ös Japán nagydíjat",
     summary: "Szerencsét hozott a fehér festés",
-    first_section: "Hiába a McLaren-ek gumielőnye",
-    last_section: "Csapattársa hazájában ennek ellenére csak a 12. helyen zárt",
+    first_section: "<p>A 2025-ös Forma–1-es szezon harmadik futamán, április 6-án, a szuzukai pályán Max Verstappen, a Red Bull Racing-Honda RBPT pilótája, rajt-cél győzelmet aratott. A holland versenyző 1:22:06.983-as idővel ért célba, mindössze 1.423 másodperccel megelőzve a McLaren-Mercedes brit pilótáját, Lando Norrist. A dobogó harmadik fokára Norris csapattársa, az ausztrál Oscar Piastri állhatott fel, aki 2.129 másodperccel maradt el Verstappentől .</p>" +
+      "<p>A verseny során egy vitatott pillanat is történt, amikor a bokszkiállások után Norris azt állította, hogy Verstappen leszorította őt a pályáról. Az esetet a versenybírák megvizsgálták, de végül nem indítottak további vizsgálatot .\n",
+    last_section: "<p>A Mercedes újonca, Andrea Kimi Antonelli a hatodik helyen végzett, és a verseny 50. körében futott 1:30.965-ös idejével új pályarekordot állított fel </p>" +
+      "<p>A verseny után a világbajnoki pontverseny élén Lando Norris áll 62 ponttal, szorosan mögötte Max Verstappen 61 ponttal, míg Oscar Piastri 49 ponttal a harmadik helyen +" +
+      "A következő futamra, a Bahreini Nagydíjra április 20-án kerül sor, ahol a versenyzők újra összemérik tudásukat a világbajnoki címért folyó küzdelemben.</p>",
     date: "2025.04.06.",
     reviewUrl: "2025-japan-nagydij-osszefoglalo",
     isReview: false
@@ -52,25 +64,18 @@ export const review_article_list: ArticleModel[] = [
     id: 1,
     url: "2025-japan-nagydij-osszefoglalo",
     title: "A felkelő nap országában sokan visszaaludtak - 2025. Japán Nagydíj",
-    summary: "Japán versenyző, japán festés",
-    first_section: "Nyert a Red Bull",
-    second_section: "Szabadedzést nem láttam",
-    third_section: "Időmérőre a Red Bull (Verstappen) megtalálta a jobb beállításokat",
-    fourth_section: "Vasárnapi felkelés nem tartott sok izgalmat",
-    last_section: "Nyert a Red Bull, McLaren továbbra is ",
-    date: "2025.04.02.",
-    isReview: true
-  },
-  {
-    id: 1,
-    url: "2025-japan-nagydij-osszefoglalo",
-    title: "A felkelő nap országában sokan visszaaludtak - 2025. Japán Nagydíj",
-    summary: "Japán versenyző, japán festés",
-    first_section: "Nyert a Red Bull",
-    second_section: "Szabadedzést nem láttam",
-    third_section: "Időmérőre a Red Bull (Verstappen) megtalálta a jobb beállításokat",
-    fourth_section: "Vasárnapi felkelés nem tartott sok izgalmat",
-    last_section: "Nyert a Red Bull, McLaren továbbra is ",
+    summary: "Visszafogott volt virágzáskor a száguldó cirkusz ",
+    first_section: "<p>A 2025-ös Forma–1-es szezon harmadik futamán, április 6-án, a suzukai pályán Max Verstappen, a Red Bull Racing-Honda RBPT pilótája, rajt-cél győzelmet aratott. A holland versenyző 1:22:06.983-as idővel ért célba, mindössze 1.423 másodperccel megelőzve a McLaren-Mercedes brit pilótáját, Lando Norrist. A dobogó harmadik fokára Norris csapattársa, az ausztrál Oscar Piastri állhatott fel, aki 2.129 másodperccel maradt el Verstappentől. </p>" +
+      "<p>A hétvége nemcsak a pályán zajlott eseményekkel, hanem azok hiányával is emlékezetessé vált. A suzukai hétvége minden ígérete ellenére kevés meglepetést tartogatott – és bár Max Verstappen ismételten dominált, a futam izgalomfaktora elmaradt a várttól.</p>",
+    second_section: "<h3>Péntek és szombat: kiütköztek az erőviszonyok</h3>"  +
+      "<p>A szabadedzések alapján sejthető volt, hogy a McLaren továbbra is a leggyorsabb autó a mezőnyben, azonban Verstappen hosszú idő után újra villogott, és egy nagyon gyors harmadik szektorral elcsípte a pole-t a papayaktól. Ezzel szemben a Ferrari és Mercedes eléggé lemaradt a riválisokhoz képest, noha Antonelli tempója kellemes meglepetés volt.</p>",
+    third_section: "<h3>Vasárnap: taktika, türelem, pozícióőrzés</h3>" +
+      "<p>A verseny rajtja tiszta volt, Verstappen pedig az első kanyarban már magabiztosan vezetett. Norris ugyan próbálkozott, de nem volt meg benne az a plusz, ami egy előzést lehetővé tett volna a Red Bull ellen. Piastri szintén jól rajtolt, de a futam jelentős részében a pozícióját védte, nem a támadás volt a cél.</p>" +
+      "<p>Az egyetlen említésre méltó konfliktus a bokszkiállások után történt, amikor Norris úgy érezte, Verstappen szabálytalanul védte a pozícióját. A versenybírák végül nem láttak okot büntetésre – de a brit pilóta frusztrációja érezhető volt a leintés után.</p>" +
+      "<p>A kerékcserék után sem változott az élmezőny sorrendje, és mivel mindenki egykiállásos taktikán volt, a McLarennek a pályán kellett megelőznie Verstappent a győzelemért. Norris el is kezdett gyorsulni, hamar 1,5 másodpercre közelítette meg Verstappent, de ennél nem nagyon tudott közelebb kerülni hozzá. Közben a közvetlenül mögötte loholó Piastri azt mondta a csapatnak, hogy neki jobb lenne a tempója, és meg tudná támadni Verstappent.</p",
+    fourth_section: "<p>Ennek ellenére a McLaren nem cserélte meg a versenyzőit, amivel viszont Verstappen malmára hajtották a vizet. Az utolsó körökben is simán tartani tudta a minimális előnyét Norrisszal szemben, ami végül elégnek bizonyult a győzelemhez. Verstappen karrierje 64. győzelmét szerezte, sorozatban negyedszerre nyert Japánban. A pontszerzők sorrendje: Verstappen, Norris, Piastri, Leclerc, Russell, Antonelli, Hamilton, Hadjar, Albon, Bearman. Nem sikerült jól a Red Bull-os bemutatkozása Tsunodának, csak 12. lett a japán versenyző.</p>",
+    last_section: "<p>A Mercedes ifjonca, Andrea Kimi Antonelli hatodik helye és új pályarekordja (1:30.965) a nap egyik legpozitívabb mozzanata volt. A verseny tempója viszont a nézők nagy részének csalódást okozott. Az online fórumokon sok japán rajongó szó szerint „visszaalvós” futamként jellemezte az eseményt – nem voltak látványos csaták, meglepő taktikai húzások, sem drámai kiesések.</p>" +
+      "<p>A világbajnoki pontverseny éleződik: Lando Norris 62 ponttal vezet, Verstappen közvetlenül mögötte 61 ponttal, Piastri pedig 49 ponttal a harmadik. A szezon eleji kiegyensúlyozottság biztató, de a Red Bull tempója nehéz dolga lesz Verstappen-nek</p>",
     date: "2025.04.02.",
     isReview: true
   },
