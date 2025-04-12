0051e988d9e430bfafa9e25e9805de6bd0c0ed11
Bence2107 <szabobence003@gmail.com>
2025-04-12 23:32:04 +0200

    Write example dummy's

--- src/app/constants/comments_list.ts
+++ src/app/constants/comments_list.ts
@@ -5,21 +5,21 @@ export const comments_list : CommentModel[] = [
     id: 1,
     articleUrl: "verstappen-nyert-japan-festesben",
     userid: 1,
-    content: "Szépen ment Max! aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaa aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaa aaaaaaaaaaaaaaaaaaaaa",
+    content: "Szépen ment Max! A McLaren-nek pedig szerintem el kéne döntenie, hogy egyszemélyes vagy csapat bajnokságot játszanak.",
     timestamp: "2025.04.03."
   },
   {
     id: 2,
     articleUrl: "verstappen-nyert-japan-festesben",
     userid: 2,
-    content: "Lol",
+    content: "Verstappen szépen csinálta és osztotta be a gumijait",
     timestamp: "2025.04.04."
   },
   {
     id: 3,
     articleUrl: "ferrari-25-attores",
     userid: 1,
-    content: "Nem jók a ferrarik",
+    content: "Nem jók a ferrarik. Szerintem koncentrálniuk kellene teljes mértékben 2026-ra, talán ott több sikert érnek el",
     timestamp: "2025.04.01."
   },
 ]
