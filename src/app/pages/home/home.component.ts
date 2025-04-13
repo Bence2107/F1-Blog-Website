9ba6f3ba94dc618f5adcd989ddb7e2b235873207
Bence2107 <szabobence003@gmail.com>
2025-04-13 11:36:59 +0200

    Make Welcome section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PrimaryNewsListComponent} from './components/primary-news-list/primary-news-list.component';
+import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
     CommonModule,
     PrimaryNewsListComponent,
+    MatButton,
+    RouterLink,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
