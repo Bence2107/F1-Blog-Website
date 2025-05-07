66d63735132a4f497b45cbce94e8a33aa51ecda6
Bence2107 <szabobence003@gmail.com>
2025-05-07 17:56:00 +0200

    Rework Login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,12 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButton} from '@angular/material/button';
 import {Router, RouterLink} from '@angular/router';
-import {AuthService} from '../auth_service';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {CustomsnackbarComponent} from '../../../components/customsnackbar/customsnackbar.component';
+import {AuthService} from '../../../services/auth/auth.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -19,37 +20,77 @@ import {CustomsnackbarComponent} from '../../../components/customsnackbar/custom
     ReactiveFormsModule,
     MatButton,
     RouterLink,
+    NgIf,
   ],
 })
 export class LoginComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
   loginForm: FormGroup;
+  loginError: string = "";
+  userData: any;
 
-  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router,  private snackBar: MatSnackBar) {
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router, private snackBar: MatSnackBar) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
     });
   }
 
   login() {
-    this.auth.login(2) //login dummy with id (not checking all users)
-
-    this.snackBar.openFromComponent(CustomsnackbarComponent, {
-      data: { message: 'Sikeres Bejelentkezés', actionLabel: 'Rendben' },
-      duration: 3000,
-      horizontalPosition: 'center',
-    });
-  }
-
-  onSubmit() {
-    if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
+    if(this.email.invalid) {
+      this.loginError = "Kérem, helyes Email-t adjon meg!";
+      return;
     }
+
+    if (this.password.invalid) {
+      this.loginError = "A jelszó 6 karakter";
+      return;
+    }
+
+    const emailValue = this.email.value || '';
+    const passwordValue = this.password.value || '';
+
+    this.auth.signIn(emailValue, passwordValue)
+      .then(userCredential => {
+        console.log("Login Success: " + userCredential.user);
+        return this.auth.getUserById(userCredential.user.uid);
+      })
+      .then(userData => {
+        this.auth.updateLogInStatus(true);
+        this.router.navigate(['/home']);
+        localStorage.setItem("userId", userData.id);
+        this.snackBar.openFromComponent(CustomsnackbarComponent, {
+          data: { message: 'Sikeres Bejelentkezés', actionLabel: 'Rendben' },
+          duration: 3000,
+          horizontalPosition: 'center',
+        });
+      })
+      .catch(error => {
+        console.log("Login Error: " + error);
+        switch (error.code) {
+          case 'auth/user-not-found':
+            this.loginError = 'Nincs ilyen felhasználó';
+            break;
+          case 'auth/wrong-password':
+            this.loginError = 'Helytelen jelszó';
+            break;
+          case 'auth/invalid-credential':
+            this.loginError = 'Helytelen Email vagy jelszó!';
+            break;
+          default:
+            this.loginError = "Hiba történt, kérem próbálja újra!";
+        }
+      });
   }
 
   ngOnInit(): void {
-    if(this.auth.getLoggedInStatus()){
-      this.router.navigate(['/profile', this.auth.getLoggedId()]);
+    const userId = localStorage.getItem('userId');
+    if(userId) {
+      this.auth.getUserById(userId).then((user) => {
+        this.userData = user;
+      });
+    }
+    if (this.userData || this.auth.isLoggedIn()) {
+      this.router.navigate(["/login"]);
     }
   }
 }
